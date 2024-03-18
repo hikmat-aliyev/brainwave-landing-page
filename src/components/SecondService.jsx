3e3f2ae154da95311d3b6326101d7e1dcc108268
Hikmat Ali <hikmat.aliyevm@gmail.com>
2024-03-18 15:46:41 +0100

    Create second service component

---
+++ src/components/SecondService.jsx
@@ -0,0 +1,12 @@
+import {
+  Gradient,
+  PhotChatMessage,
+  VideoChatMessage,
+  VideoBar,
+} from "../design/Services";
+
+function SecondService() {
+  return <div>SecondService</div>;
+}
+
+export default SecondService;
