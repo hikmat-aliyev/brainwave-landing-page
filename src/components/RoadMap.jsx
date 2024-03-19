3048f96400e021501b180a137e4ba85c60d027ee
Hikmat Ali <hikmat.aliyevm@gmail.com>
2024-03-19 13:04:28 +0100

    Create all 4 roadmaps and pass relevant props

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -5,9 +5,64 @@ import SingleRoadMap from "./SingleRoadMap";
 import { roadmap1, roadmap2, roadmap3, roadmap4 } from "../assets";
 
 function RoadMap() {
+  const defaultBorder = "border border-gray-700 rounded-[20px]";
+
   return (
-    <div className="relative border-2 border-red-600">
-      <SingleRoadMap status={"DONE"} statusImg={check2} roadmapImg={roadmap1} />
+    <div className="relative flex flex-col mt-60 gap-0">
+      <div className="flex flex-col w-full justify-center items-center">
+        <p className="text-[0.7rem] text-gray-400 tracking-widest">
+          [ READY TO GET STARTED ]
+        </p>
+        <h1 className="h2">What we’re working on</h1>
+      </div>
+
+      <div className="flex items-center justify-center gap-4">
+        <div className="w-[42vw] flex flex-col gap-4">
+          <SingleRoadMap
+            status={"DONE"}
+            statusImg={check2}
+            roadmapImg={roadmap1}
+            roadmapName={"Voice recognition"}
+            roadmapDescription={
+              "Enable the chatbot to understand and respond to voice commands, making it easier for users to interact with the app hands-free."
+            }
+            containerBorder={defaultBorder}
+          />
+          <SingleRoadMap
+            status={"DONE"}
+            statusImg={check2}
+            roadmapImg={roadmap3}
+            roadmapName={"Chatbot customization"}
+            roadmapDescription={
+              "Allow users to customize the chatbot's appearance and behavior, making it more engaging and fun to interact with."
+            }
+            containerBorder={defaultBorder}
+          />
+        </div>
+        <div className="w-[42vw] mt-60 flex flex-col gap-4">
+          <SingleRoadMap
+            status={"IN PROGRESS"}
+            statusImg={loading1}
+            roadmapImg={roadmap2}
+            roadmapName={"Gamification"}
+            roadmapDescription={
+              "Add game-like elements, such as badges or leaderboards, to incentivize users to engage with the chatbot more frequently."
+            }
+            containerBorder={defaultBorder}
+          />
+          <SingleRoadMap
+            status={"IN PROGRESS"}
+            statusImg={loading1}
+            roadmapImg={roadmap4}
+            roadmapName={"Integration with APIs"}
+            roadmapDescription={
+              "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations."
+            }
+            containerBorder={defaultBorder}
+          />
+        </div>
+      </div>
+
       <Gradient />
     </div>
   );
--- src/components/SingleRoadMap.jsx
+++ src/components/SingleRoadMap.jsx
@@ -1,10 +1,22 @@
 import "../styles/RoadMaps.css";
 
-function SingleRoadMap({ status, statusImg, roadmapImg }) {
+function SingleRoadMap({
+  status,
+  statusImg,
+  roadmapImg,
+  roadmapName,
+  roadmapDescription,
+  containerBorder,
+}) {
   return (
-    <div id="singleRoadMapContainer" className="w-[46vw] border flex flex-col">
+    <div
+      id="singleRoadMapContainer"
+      className={`w-full flex flex-col rounded-[40px] ${containerBorder}`}
+    >
       <div className="flex justify-between w-full px-16 py-15">
-        <p>[ MAY 2023 ]</p>
+        <p className="text-gray-400 text-[0.7rem] tracking-widest font-thin">
+          [ MAY 2023 ]
+        </p>
         <div
           className="w-fit bg-white text-black flex justify-around gap-3 rounded-[5px]
                      px-4 font-thin text-[0.7rem]"
@@ -15,6 +27,11 @@ function SingleRoadMap({ status, statusImg, roadmapImg }) {
       </div>
 
       <img src={roadmapImg} alt={roadmapImg} />
+
+      <div className="flex flex-col px-16 py-8 pb-20 gap-6">
+        <h4 className="h4">{roadmapName}</h4>
+        <p className="text-gray-500 font-thin">{roadmapDescription}</p>
+      </div>
     </div>
   );
 }
